1b783e80086b5f8216164cc9745d7d3f200dd0f5
jmaksimik <jmaks@me.com>
2024-07-01 20:35:43 -0400

    work on car details

--- components/index.ts
+++ components/index.ts
@@ -6,6 +6,7 @@ import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
 import SearchManufacturer from './SearchManufacturer';
 import CarCard from './CarCard';
+import CarDetails from './CarDetails';
 
 
 export {
@@ -16,5 +17,6 @@ export {
     SearchBar,
     CustomFilter,
     SearchManufacturer,
-    CarCard
+    CarCard,
+    CarDetails
 }
